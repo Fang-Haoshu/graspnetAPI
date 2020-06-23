ca51603bf0d34bace4d207dc50be5d4a57c1c02b
Fang-Haoshu <510641426@qq.com>
2020-06-23 20:55:23 +0800

    init

---
+++ utils.py
@@ -0,0 +1,298 @@
+import os
+import open3d as o3d
+import numpy as np
+from PIL import Image
+from xmlhandler import xmlReader
+from transforms3d.euler import euler2mat
+
+
+
+def generate_views(N, phi=(np.sqrt(5)-1)/2, center=np.zeros(3, dtype=np.float32), R=1):
+    idxs = np.arange(N, dtype=np.float32)
+    Z = (2 * idxs + 1) / N - 1
+    X = np.sqrt(1 - Z**2) * np.cos(2 * idxs * np.pi * phi)
+    Y = np.sqrt(1 - Z**2) * np.sin(2 * idxs * np.pi * phi)
+    views = np.stack([X,Y,Z], axis=1)
+    views = R * np.array(views) + center
+    return views
+
+def generate_scene_model(dataset_root, scene_name, anno_idx, return_poses=False, align=False, camera='realsense'):
+
+    if align:
+        camera_poses = np.load(os.path.join(dataset_root, 'scenes', scene_name, camera, 'camera_poses.npy'))
+        camera_pose = camera_poses[anno_idx]
+        align_mat = np.load(os.path.join(dataset_root, 'scenes', scene_name, camera, 'cam0_wrt_table.npy'))
+        camera_pose = np.matmul(align_mat,camera_pose)
+    print('Scene {}, {}'.format(scene_name, camera))
+    scene_reader = xmlReader(os.path.join(dataset_root, 'scenes', scene_name, camera, 'annotations', '%04d.xml'%anno_idx))
+    posevectors = scene_reader.getposevectorlist()
+    obj_list = []
+    mat_list = []
+    model_list = []
+    pose_list = []
+    for posevector in posevectors:
+        obj_idx, pose = parse_posevector(posevector)
+        obj_list.append(obj_idx)
+        mat_list.append(pose)
+
+    for obj_idx, pose in zip(obj_list, mat_list):
+        plyfile = os.path.join(dataset_root, 'models', '%03d'%obj_idx, 'nontextured.ply')
+        model = o3d.io.read_point_cloud(plyfile)
+        points = np.array(model.points)
+        if align:
+            pose = np.dot(camera_pose, pose)
+        points = transform_points(points, pose)
+        model.points = o3d.utility.Vector3dVector(points)
+        model_list.append(model)
+        pose_list.append(pose)
+
+    if return_poses:
+        return model_list, obj_list, pose_list
+    else:
+        return model_list
+
+def generate_scene_pointcloud(dataset_root, scene_name, anno_idx, align=False, camera='kinect'):
+    colors = np.array(Image.open(os.path.join(dataset_root, 'scenes', scene_name, camera, 'rgb', '%04d.png'%anno_idx)), dtype=np.float32) / 255.0
+    depths = np.array(Image.open(os.path.join(dataset_root, 'scenes', scene_name, camera, 'depth', '%04d.png'%anno_idx)))
+    intrinsics = np.load(os.path.join(dataset_root, 'scenes', scene_name, camera, 'camK.npy'))
+    fx, fy = intrinsics[0,0], intrinsics[1,1]
+    cx, cy = intrinsics[0,2], intrinsics[1,2]
+    s = 1000.0
+    
+    if align:
+        camera_poses = np.load(os.path.join(dataset_root, 'scenes', scene_name, camera, 'camera_poses.npy'))
+        camera_pose = camera_poses[anno_idx]
+        align_mat = np.load(os.path.join(dataset_root, 'scenes', scene_name, camera, 'cam0_wrt_table.npy'))
+        camera_pose = align_mat.dot(camera_pose)
+
+    xmap, ymap = np.arange(colors.shape[1]), np.arange(colors.shape[0])
+    xmap, ymap = np.meshgrid(xmap, ymap)
+
+    points_z = depths / s
+    points_x = (xmap - cx) / fx * points_z
+    points_y = (ymap - cy) / fy * points_z
+
+    mask = (points_z > 0)
+    points = np.stack([points_x, points_y, points_z], axis=-1)
+    points = points[mask]
+    colors = colors[mask]
+    if align:
+        points = transform_points(points, camera_pose)
+
+    cloud = o3d.geometry.PointCloud()
+    cloud.points = o3d.utility.Vector3dVector(points)
+    cloud.colors = o3d.utility.Vector3dVector(colors)
+
+    return cloud
+
+def rotation_matrix(rx, ry, rz):
+    Rx = np.array([[1,          0,           0],
+                   [0, np.cos(rx), -np.sin(rx)],
+                   [0, np.sin(rx),  np.cos(rx)]])
+    Ry = np.array([[ np.cos(ry), 0, np.sin(ry)],
+                   [          0, 1,          0],
+                   [-np.sin(ry), 0, np.cos(ry)]])
+    Rz = np.array([[np.cos(rz), -np.sin(rz), 0],
+                   [np.sin(rz),  np.cos(rz), 0],
+                   [         0,           0, 1]])
+    R = Rz.dot(Ry).dot(Rx)
+    return R
+
+def transform_matrix(tx, ty, tz, rx, ry, rz):
+    trans = np.eye(4)
+    trans[:3,3] = np.array([tx, ty, tz])
+    rot_x = np.array([[1,          0,           0],
+                      [0, np.cos(rx), -np.sin(rx)],
+                      [0, np.sin(rx),  np.cos(rx)]])
+    rot_y = np.array([[ np.cos(ry), 0, np.sin(ry)],
+                      [          0, 1,          0],
+                      [-np.sin(ry), 0, np.cos(ry)]])
+    rot_z = np.array([[np.cos(rz), -np.sin(rz), 0],
+                      [np.sin(rz),  np.cos(rz), 0],
+                      [         0,           0, 1]])
+    trans[:3,:3] = rot_x.dot(rot_y).dot(rot_z)
+    return trans
+
+def matrix_to_dexnet_params(matrix):
+    approach = matrix[:, 0]
+    binormal = matrix[:, 1]
+    axis_y = binormal
+    axis_x = np.array([axis_y[1], -axis_y[0], 0])
+    if np.linalg.norm(axis_x) == 0:
+        axis_x = np.array([1, 0, 0])
+    axis_x = axis_x / np.linalg.norm(axis_x)
+    axis_y = axis_y / np.linalg.norm(axis_y)
+    axis_z = np.cross(axis_x, axis_y)
+    R = np.c_[axis_x, np.c_[axis_y, axis_z]]
+    approach = R.T.dot(approach)
+    cos_t, sin_t = approach[0], -approach[2]
+    angle = np.arccos(cos_t)
+    if sin_t < 0:
+        angle = np.pi * 2 - angle
+    return binormal, angle
+
+def viewpoint_params_to_matrix(towards, angle):
+    axis_x = towards
+    axis_y = np.array([-axis_x[1], axis_x[0], 0])
+    if np.linalg.norm(axis_y) == 0:
+        axis_y = np.array([0, 1, 0])
+    axis_x = axis_x / np.linalg.norm(axis_x)
+    axis_y = axis_y / np.linalg.norm(axis_y)
+    axis_z = np.cross(axis_x, axis_y)
+    R1 = np.array([[1, 0, 0],
+                   [0, np.cos(angle), -np.sin(angle)],
+                   [0, np.sin(angle), np.cos(angle)]])
+    R2 = np.c_[axis_x, np.c_[axis_y, axis_z]]
+    matrix = R2.dot(R1)
+    return matrix
+
+def batch_viewpoint_params_to_matrix(batch_towards, batch_angle):
+    axis_x = batch_towards
+    ones = np.ones(axis_x.shape[0], dtype=axis_x.dtype)
+    zeros = np.zeros(axis_x.shape[0], dtype=axis_x.dtype)
+    axis_y = np.stack([-axis_x[:,1], axis_x[:,0], zeros], axis=-1)
+    mask_y = (np.linalg.norm(axis_y, axis=-1) == 0)
+    axis_y[mask_y] = np.array([0, 1, 0])
+    axis_x = axis_x / np.linalg.norm(axis_x, axis=-1, keepdims=True)
+    axis_y = axis_y / np.linalg.norm(axis_y, axis=-1, keepdims=True)
+    axis_z = np.cross(axis_x, axis_y)
+    sin = np.sin(batch_angle)
+    cos = np.cos(batch_angle)
+    R1 = np.stack([ones, zeros, zeros, zeros, cos, -sin, zeros, sin, cos], axis=-1)
+    R1 = R1.reshape([-1,3,3])
+    R2 = np.stack([axis_x, axis_y, axis_z], axis=-1)
+    matrix = np.matmul(R2, R1)
+    return matrix.astype(np.float32)
+
+def dexnet_params_to_matrix(binormal, angle):
+    axis_y = binormal
+    axis_x = np.array([axis_y[1], -axis_y[0], 0])
+    if np.linalg.norm(axis_x) == 0:
+        axis_x = np.array([1, 0, 0])
+    axis_x = axis_x / np.linalg.norm(axis_x)
+    axis_y = axis_y / np.linalg.norm(axis_y)
+    axis_z = np.cross(axis_x, axis_y)
+    R1 = np.array([[np.cos(angle), 0, np.sin(angle)],
+                  [0, 1, 0],
+                  [-np.sin(angle), 0, np.cos(angle)]])
+    R2 = np.c_[axis_x, np.c_[axis_y, axis_z]]
+    matrix = R2.dot(R1)
+    return matrix
+
+def transform_points(points, trans):
+    ones = np.ones([points.shape[0],1], dtype=points.dtype)
+    points_ = np.concatenate([points, ones], axis=-1)
+    points_ = np.matmul(trans, points_.T).T
+    return points_[:,:3]
+
+def get_model_grasps(datapath):
+    label = np.load(datapath)
+    points = label['points']
+    offsets = label['offsets']
+    scores = label['scores']
+    collision = label['collision']
+    return points, offsets, scores, collision
+
+def parse_posevector(posevector):
+    mat = np.zeros([4,4],dtype=np.float32)
+    alpha, beta, gamma = posevector[4:7]
+    alpha = alpha / 180.0 * np.pi
+    beta = beta / 180.0 * np.pi
+    gamma = gamma / 180.0 * np.pi
+    mat[:3,:3] = euler2mat(alpha, beta, gamma)
+    mat[:3,3] = posevector[1:4]
+    mat[3,3] = 1
+    obj_idx = int(posevector[0])
+    return obj_idx, mat
+
+def create_mesh_box(width, height, depth, dx=0, dy=0, dz=0):
+    box = o3d.geometry.TriangleMesh()
+    vertices = np.array([[0,0,0],
+                         [width,0,0],
+                         [0,0,depth],
+                         [width,0,depth],
+                         [0,height,0],
+                         [width,height,0],
+                         [0,height,depth],
+                         [width,height,depth]])
+    vertices[:,0] += dx
+    vertices[:,1] += dy
+    vertices[:,2] += dz
+    triangles = np.array([[4,7,5],[4,6,7],[0,2,4],[2,6,4],
+                          [0,1,2],[1,3,2],[1,5,7],[1,7,3],
+                          [2,3,7],[2,7,6],[0,4,1],[1,4,5]])
+    box.vertices = o3d.utility.Vector3dVector(vertices)
+    box.triangles = o3d.utility.Vector3iVector(triangles)
+    return box
+
+def plot_gripper_pro_max(center, R, width, depth, score=1):
+    '''
+        center: target point
+        R: rotation matrix
+    '''
+    x, y, z = center
+    height=0.004
+    finger_width = 0.004
+    tail_length = 0.04
+    depth_base = 0.02
+    
+    color_r = score # red for high score
+    color_b = 1 - score # blue for low score
+    color_g = 0
+    left = create_mesh_box(depth+depth_base+finger_width, finger_width, height)
+    right = create_mesh_box(depth+depth_base+finger_width, finger_width, height)
+    bottom = create_mesh_box(finger_width, width, height)
+    tail = create_mesh_box(tail_length, finger_width, height)
+
+    left_points = np.array(left.vertices)
+    left_triangles = np.array(left.triangles)
+    left_points[:,0] -= depth_base + finger_width
+    left_points[:,1] -= width/2 + finger_width
+    left_points[:,2] -= height/2
+
+    right_points = np.array(right.vertices)
+    right_triangles = np.array(right.triangles) + 8
+    right_points[:,0] -= depth_base + finger_width
+    right_points[:,1] += width/2
+    right_points[:,2] -= height/2
+
+    bottom_points = np.array(bottom.vertices)
+    bottom_triangles = np.array(bottom.triangles) + 16
+    bottom_points[:,0] -= finger_width + depth_base
+    bottom_points[:,1] -= width/2
+    bottom_points[:,2] -= height/2
+
+    tail_points = np.array(tail.vertices)
+    tail_triangles = np.array(tail.triangles) + 24
+    tail_points[:,0] -= tail_length + finger_width + depth_base
+    tail_points[:,1] -= finger_width / 2
+    tail_points[:,2] -= height/2
+
+    vertices = np.concatenate([left_points, right_points, bottom_points, tail_points], axis=0)
+    vertices = np.dot(R, vertices.T).T + center
+    triangles = np.concatenate([left_triangles, right_triangles, bottom_triangles, tail_triangles], axis=0)
+    colors = np.array([ [color_r,color_g,color_b] for _ in range(len(vertices))])
+
+    gripper = o3d.geometry.TriangleMesh()
+    gripper.vertices = o3d.utility.Vector3dVector(vertices)
+    gripper.triangles = o3d.utility.Vector3iVector(triangles)
+    gripper.vertex_colors = o3d.utility.Vector3dVector(colors)
+    return gripper
+
+
+def find_scene_by_model_id(dataset_root, model_id_list):
+    picked_scene_names = []
+    scene_names = ['scene_'+str(i).zfill(4) for i in range(190)]
+    for scene_name in scene_names:
+        try:
+            scene_reader = xmlReader(os.path.join(dataset_root, 'scenes', scene_name, 'kinect', 'annotations', '0000.xml'))
+        except:
+            continue
+        posevectors = scene_reader.getposevectorlist()
+        for posevector in posevectors:
+            obj_idx, _ = parse_posevector(posevector)
+            if obj_idx in model_id_list:
+                picked_scene_names.append(scene_name)
+                print(obj_idx, scene_name)
+                break
+    return picked_scene_names
---
+++ vis.py
@@ -0,0 +1,122 @@
+import os
+import time
+import numpy as np
+import open3d as o3d
+from transforms3d.euler import euler2mat, quat2mat
+from utils import generate_scene_model, generate_scene_pointcloud, generate_views, get_model_grasps, plot_gripper_pro_max, transform_points
+from rotation import viewpoint_params_to_matrix, batch_viewpoint_params_to_matrix
+
+def create_table_cloud(width, height, depth, dx=0, dy=0, dz=0, grid_size=0.01):
+    xmap = np.linspace(0, width, int(width/grid_size))
+    ymap = np.linspace(0, depth, int(depth/grid_size))
+    zmap = np.linspace(0, height, int(height/grid_size))
+    xmap, ymap, zmap = np.meshgrid(xmap, ymap, zmap, indexing='xy')
+    xmap += dx
+    ymap += dy
+    zmap += dz
+    # points = np.stack([xmap, ymap, zmap], axis=-1)
+    points = np.stack([xmap, -ymap, -zmap], axis=-1)
+    points = points.reshape([-1, 3])
+    cloud = o3d.geometry.PointCloud()
+    cloud.points = o3d.utility.Vector3dVector(points)
+    return cloud
+
+
+def visAnno(dataset_root, scene_name, anno_idx, camera, ymin=-0.1, ymax=0.1, num_grasp=10, th=0.3, align_to_table=True, max_width=0.08, save_folder='save_fig', show=False):
+    model_list, obj_list, pose_list = generate_scene_model(dataset_root, scene_name, anno_idx, return_poses=True, align=align_to_table, camera=camera)
+    point_cloud = generate_scene_pointcloud(dataset_root, scene_name, anno_idx, align=align_to_table, camera='kinect')
+
+    table = create_table_cloud(1.0, 0.02, 1.0, dx=-0.5, dy=-0.5, dz=0, grid_size=0.01)
+    num_views, num_angles, num_depths = 300, 12, 4
+    views = generate_views(num_views)
+    collision_label = np.load('{}/collision_label/{}/collision_labels.npz'.format(dataset_root,scene_name))
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(width = 1280, height = 720)
+    ctr = vis.get_view_control()
+    param = o3d.io.read_pinhole_camera_parameters('param_{}.json'.format(camera))
+
+    if align_to_table:
+        cam_pos = np.load(os.path.join(dataset_root, 'scenes', scene_name, camera, 'cam0_wrt_table.npy'))
+        param.extrinsic = np.linalg.inv(cam_pos).tolist()
+
+    grippers = []
+    for i, (obj_idx, trans) in enumerate(zip(obj_list, pose_list)):
+        sampled_points, offsets, scores, _ = get_model_grasps('%s/grasp_label/%03d_labels.npz'%(dataset_root, obj_idx))
+        collision = collision_label['arr_{}'.format(i)]
+
+        cnt = 0
+        point_inds = np.arange(sampled_points.shape[0])
+        np.random.shuffle(point_inds)
+        
+        gripper_params = []
+
+        for point_ind in point_inds:
+            target_point = sampled_points[point_ind]
+            offset = offsets[point_ind]
+            score = scores[point_ind]
+            if target_point[1] < ymin or target_point[1] > ymax:
+                continue
+            view_inds = np.arange(300)
+            np.random.shuffle(view_inds)
+            flag = False
+            for v in view_inds:
+                if flag: break
+                view = views[v]
+                angle_inds = np.arange(12)
+                np.random.shuffle(angle_inds)
+                for a in angle_inds:
+                    if flag: break
+                    depth_inds = np.arange(4)
+                    np.random.shuffle(depth_inds)
+                    for d in depth_inds:
+                        if flag: break
+                        angle, depth, width = offset[v, a, d]
+                        if score[v, a, d] > th or score[v, a, d] < 0:
+                            continue
+                        if width > max_width:
+                            continue
+                        if collision[point_ind, v, a, d]:
+                            continue
+                        R = viewpoint_params_to_matrix(-view, angle)
+                        t = transform_points(target_point[np.newaxis,:], trans).squeeze()
+                        R = np.dot(trans[:3,:3], R)
+                        gripper = plot_gripper_pro_max(t, R, width, depth, 1.1-score[v, a, d])
+                        grippers.append(gripper)
+                        gripper_params.append([target_point[0],target_point[1],target_point[2],view[0],view[1],view[2],angle,depth,width])
+                        flag = True
+            if flag:
+                cnt += 1
+            if cnt == num_grasp:
+                break
+
+    vis.add_geometry(point_cloud)
+    for gripper in grippers:
+        vis.add_geometry(gripper)
+    ctr.convert_from_pinhole_camera_parameters(param)
+    vis.poll_events()
+    filename = os.path.join(save_folder, '{}_{}_pointcloud.png'.format(scene_name, camera))
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+    vis.capture_screen_image(filename, do_render=True)
+    if show:
+        o3d.visualization.draw_geometries([point_cloud, *grippers])
+
+
+    vis.remove_geometry(point_cloud)
+    vis.add_geometry(table)
+    for model in model_list:
+        vis.add_geometry(model)
+    ctr.convert_from_pinhole_camera_parameters(param)
+    vis.poll_events()
+    filename = os.path.join(save_folder, '{}_{}_model.png'.format(scene_name, camera))
+    vis.capture_screen_image(filename, do_render=True)
+    if show:
+        o3d.visualization.draw_geometries([table, *model_list, *grippers])
+
+if __name__ == '__main__':
+    camera = 'kinect'
+    dataset_root = '../'
+    scene_name = 'scene_0000'
+    anno_idx = 0
+    visAnno(dataset_root, scene_name, anno_idx, camera, ymin=-0.1, ymax=0.1, num_grasp=1, th=1.1, align_to_table=True, max_width=0.08, save_folder='save_fig', show=False)
