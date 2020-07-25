4837b244f6649b753ae572ce473654e5f82aa2dc
Fang-Haoshu <510641426@qq.com>
2020-07-25 21:04:55 +0800

    add func

--- utils.py
+++ utils.py
@@ -5,7 +5,28 @@ from PIL import Image
 from xmlhandler import xmlReader
 from transforms3d.euler import euler2mat
 
+class CameraInfo():
+    def __init__(self, width, height, fx, fy, cx, cy, scale):
+        self.width = width
+        self.height = height
+        self.fx = fx
+        self.fy = fy
+        self.cx = cx
+        self.cy = cy
+        self.scale = scale
 
+def create_point_cloud_from_depth_image(depth, camera, organized=True):
+    assert(depth.shape[0] == camera.height and depth.shape[1] == camera.width)
+    xmap = np.arange(camera.width)
+    ymap = np.arange(camera.height)
+    xmap, ymap = np.meshgrid(xmap, ymap)
+    points_z = depth / camera.scale
+    points_x = (xmap - camera.cx) * points_z / camera.fx
+    points_y = (ymap - camera.cy) * points_z / camera.fy
+    cloud = np.stack([points_x, points_y, points_z], axis=-1)
+    if not organized:
+        cloud = cloud.reshape([-1, 3])
+    return cloud
 
 def generate_views(N, phi=(np.sqrt(5)-1)/2, center=np.zeros(3, dtype=np.float32), R=1):
     idxs = np.arange(N, dtype=np.float32)
